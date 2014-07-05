92811e6d3a6af01b30382b5f432fcaa5d64f182c
dmt.20102514@gmail.com <dmt.20102514@gmail.com@1d1aefd8-5879-4c96-91ef-7cd83f31b314>
2014-07-05 12:40:41 +0000

    Checkin:     Kiểm tra url đã được phân quyền? Phạm vi: cho tất cả các form

--- trunk/ han-chinh-topica/SourceCode/QuanLyTaiSan/Site.master.cs
+++ trunk/ han-chinh-topica/SourceCode/QuanLyTaiSan/Site.master.cs
@@ -42,10 +42,11 @@ public partial class SiteMaster : System.Web.UI.MasterPage
                         // Lấy toàn bộ các menu cấp 1 được cấp quyền và được hiển thị
                         rptMainMenu.DataSource = m_ds_ht_chuc_nang.HT_CHUC_NANG.Select("CHUC_NANG_PARENT_ID IS NULL AND HIEN_THI_YN='Y'", "VI_TRI");
                         rptMainMenu.DataBind();
-                        //if (!Person.check_user_have_menu())
-                        //{
-                        //    Response.Redirect("/QuanLyTaiSan/Default.aspx", false);
-                        //}
+                        // kiem tra url đã được phân quyền chưa, nếu chưa thì quay về trang chủ
+                        if (!Person.check_user_have_menu())
+                        {
+                            Response.Redirect("/QuanLyTaiSan/Default.aspx", false);
+                        }
                     }
                 }
                 else
